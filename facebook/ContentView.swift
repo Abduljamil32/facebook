08da158ce5d89f483f79fc95fd14dc9f73ab248e
co.abu <anizomxoja@gmail.com>
2022-04-24 15:24:48 +0500

    Initial Commit

---
+++ facebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  facebook
+//
+//  Created by Avaz Mukhitdinov on 24/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ facebook/facebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookApp.swift
+//  facebook
+//
+//  Created by Avaz Mukhitdinov on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct facebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
